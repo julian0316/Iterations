59f95eca8ab4d75255b0ceed155e93433cefec2b
julian heredia <julian.heredia85@gmail.com>
2019-09-10 21:03:24 -0400

    Second In-class exercise

---
+++ Iterations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your years of experience");
+            int a = int.Parse(Console.ReadLine());
+            int i = 0;
+                while (i <= a)
+           
+            {
+                Console.WriteLine("You have {0} years experience",i);
+                i++;
+            }
+        }
+
+    }
+}
